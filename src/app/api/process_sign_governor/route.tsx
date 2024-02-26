fd544216e77cd2abc3209f6ba9e6423624eaf8ec
Hrithik Sampson <hrithikedwardsampson@gmail.com>
2024-02-26 09:58:01 +0530

    feat: governor test

--- src/app/api/process_sign_governor/route.tsx
+++ src/app/api/process_sign_governor/route.tsx
@@ -4,29 +4,29 @@ import { headers } from "next/headers";
 import { Metadata, ResolvingMetadata } from "next";
 export const runtime = "edge";
 export const dynamic = 'force-dynamic';
-export async function generateMetadata(
-  {},
-  parent: ResolvingMetadata
-): Promise<Metadata> {
+// export async function generateMetadata(
+//   {},
+//   parent: ResolvingMetadata
+// ): Promise<Metadata> {
  
-  const host = headers().get("host");
-  const protocol = process?.env.NODE_ENV === "development" ? "http" : "https";
+//   const host = headers().get("host");
+//   const protocol = process?.env.NODE_ENV === "development" ? "http" : "https";
   
-  return {
-    title: 'Total Value Locked Homepage',
-    openGraph: {
-      images: [{
-        url: `${protocol}://${host}/api/governors`,
-        width: 1200,
-        height: 630
-      }],
-    },
-    other: {
-      'fc:frame': 'vNext',
-      'fc:frame:image': `${protocol}://${host}/api/governors`,
-    }
-  }
-}//return new NextResponse(`<div></div>`,{status:200, headers: { 'content-type': 'text/html' } });
+//   return {
+//     title: 'Total Value Locked Homepage',
+//     openGraph: {
+//       images: [{
+//         url: `${protocol}://${host}/api/governors`,
+//         width: 1200,
+//         height: 630
+//       }],
+//     },
+//     other: {
+//       'fc:frame': 'vNext',
+//       'fc:frame:image': `${protocol}://${host}/api/governors`,
+//     }
+//   }
+// }//return new NextResponse(`<div></div>`,{status:200, headers: { 'content-type': 'text/html' } });
   
 export async function POST(req: NextRequest) {
   const host = req.headers.get("host");
